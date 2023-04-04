359949f949bbb18237cf1afc4671f51ea8e8672f
Saurabh <saurabhsharma708@gmail.com>
2023-04-04 17:05:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/habit/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.habit
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import com.example.habit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ivAdd.setOnClickListener {
+
+        }
+    }
+}
